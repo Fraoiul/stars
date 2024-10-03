62200de1c8bc95682b4805f01d371183b299896c
chenxing <106280328@qq.com>
2024-10-03 23:26:09 +0800

    14_animated-navigation

---
+++ 50Projectsin50days/14_animated-navigation/script.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.toggle')
+const container = document.querySelector('.container')
+toggle.addEventListener('click', () => {
+    container.classList.toggle('reduceWidth')
+})
