1cd2302d8b6a867e3b2bd71e6b59d23533ab3149
frank <1062803282@qq.com>
2024-10-06 23:36:23 +0800

    提交17_movie-app

--- 50Projectsin50days/04_hidden-search/script.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelector('button')
-let ipt = document.querySelector('input')
-let search = document.querySelector('.search')
-btn.addEventListener('click', () => {
-    search.classList.toggle('getWidth')
-    ipt.focus()
-})
--- 50Projectsin50days/14_animated-navigation/script.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.querySelector('.toggle')
-const container = document.querySelector('.container')
-toggle.addEventListener('click', () => {
-    container.classList.toggle('reduceWidth')
-})
